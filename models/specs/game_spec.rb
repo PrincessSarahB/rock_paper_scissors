094b7dbde4b3e3004b0d0baa9a9a852a5c6127ca
Sarah Bartlett <sarah.bartlett28@gmail.com>
2018-04-16 19:37:26 +0100

    uploading to github

--- models/specs/game_spec.rb
+++ models/specs/game_spec.rb
@@ -21,9 +21,9 @@ def test_draw__both_scissors()
 assert_equal("it's a draw!", @game1.rock_paper_scissors("scissors", "scissors"))
 end
 
-# def test_rock_wins()
-#
-# end
+def test_rock_wins()
+assert_equal("player 1 wins! Rock beats scissors", @game1.rock_paper_scissors("rock", "scissors") )
+end
 #
 # def test_scissors_wins()
 #
